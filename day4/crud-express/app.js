ab2e90c9b5aa27ba3c35c25ac10383f81de46ead
at358683106 <mxd_358683106@163.com>
2020-07-26 22:36:51 +0800

    feat:crud 布局

--- day4/crud-express/app.js
+++ day4/crud-express/app.js
@@ -1,3 +1,4 @@
+var fs = require("fs");
 var express = require("express");
 var app = express();
 
@@ -6,7 +7,10 @@ app.use("/public/", express.static("./public"));
 app.engine("html", require("express-art-template"));
 
 app.get("/", (req, res) => {
-  res.render("index.html");
+  fs.readFile("./db.json", "utf8", (err, data) => {
+    if (err) return res.status(500).send("Server error");
+    res.render("index.html", JSON.parse(data));
+  });
 });
 
 app.listen(3000, () => console.log("监听开始"));
