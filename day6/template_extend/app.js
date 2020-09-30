7ec4e9bab2db3418a84e3ed7801ed6f306a1f99b
at358683106 <mxd_358683106@163.com>
2020-09-30 11:54:34 +0800

    feat: 基础页面渲染

--- day6/template_extend/app.js
+++ day6/template_extend/app.js
@@ -2,11 +2,7 @@ var express = require("express");
 var path = require("path");
 var app = express();
 
-app.use("public", express.static(path.join(__dirname, "./public/")));
-app.use(
-  "node_modules",
-  express.static(path.join(__dirname, "./node_modules/"))
-);
+app.use("/node_modules", express.static(path.join(__dirname, "node_modules")));
 
 app.engine("html", require("express-art-template"));
 
